1c7ea8dc57adbfa544a66da5ad76651f39abb32a
FlorencioP <sebasale10@hotmail.com>
2020-11-24 23:03:14 -0500

    Desarrollo de interfaz para los invitados

---
+++ WebContent/JavaScript/busqueda.js
@@ -0,0 +1,37 @@
+function buscarPorCedula() {
+
+}
+
+
+
+function buscar() {
+
+    var mail = document.getElementById("textoabusc").value;
+
+
+
+    if (mail == "") {
+
+        document.getElementById("informacion").innerHTML = " <b>No se a detectado el Mail</b>";
+
+
+    } else {
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari 
+            xmlhttp = new XMLHttpRequest();
+        } else { // code for IE6, IE5 
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                //alert("llegue"); 
+                document.getElementById("informacion").innerHTML = this.responseText;
+            }
+        };
+
+        xmlhttp.open("GET", "../Ajax/peticioNumsMail.html", true);
+        xmlhttp.send();
+    }
+    return false;
+}
