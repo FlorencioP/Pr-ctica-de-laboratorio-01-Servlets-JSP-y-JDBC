3c98640a11445f5bcfae4db4916fa317352ed5da
Charly1590 <carlos19.pe@gmail.com>
2020-12-01 18:22:16 -0500

    filtros anadidos

--- src/ec/edu/ups/controlador/Filtro.java
+++ src/ec/edu/ups/controlador/Filtro.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * Servlet Filter implementation class Filtro
+ * Servlet Filter implementation class Filtroqweqwe
  */
 @WebFilter({"/Filtro","/HTMLs/User/usrVin.jsp","/HTMLs/User/usrVincreate.jsp","/HTMLs/User/MostrarDetalles.jsp",
 			"/HTMLs/User/MostrarPedidos.jsp","/HTMLs/User/MensajeExito.jsp","/HTMLs/User/ActualizarDetalles.jsp",
@@ -41,7 +41,9 @@ public class Filtro implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpSession session = ((HttpServletRequest) request).getSession();
 		if(session.getAttribute("accesos") ==  null) {
+			System.out.println("no puedes pasaaar");
 			((HttpServletResponse)response).sendRedirect("/Practica_de_laboratorio_01_Servlets_JSP_y_JDBC/index.html");
+			
 		}
 		else {
 			chain.doFilter(request, response);
